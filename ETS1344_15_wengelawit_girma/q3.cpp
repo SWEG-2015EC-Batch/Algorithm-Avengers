eacfd5b6aaf63011e281669a6a0fe95ed8b7ed2a
wengelawitgirma799 <141363872+wengelawitgirma799@users.noreply.github.com>
2023-11-17 23:59:33 +0300

    Create q3.cpp

---
+++ ETS1344_15_wengelawit_girma/q3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+
+
+char ch;
+
+
+  cout<<"enter any alphabet";
+  cin>>ch;
+  if(ch>='a' && ch<='z')
+  {
+    cout<<"the entered alphabet is in lowercase";
+    ch=ch-32;
+    cout<<"uppercase is"<<ch;
+  }
+  else
+  {
+    cout<<"the entered alphabet is in uppercase";
+    ch=ch+32;
+    cout<<"lowercase is"<<ch;
+  }
+  return 0;
+}
