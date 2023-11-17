fd47752cc8c8163ad3eb49355b2a4c9868f4269d
wengelawitgirma799 <141363872+wengelawitgirma799@users.noreply.github.com>
2023-11-18 00:19:13 +0300

    Create q6.cpp

---
+++ ETS1344_15_wengelawit_girma/q6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+ using namespace std;
+int main()
+{
+ float file_size, transmission_time;
+ cout<<" enter the size of the file ";
+ cin>>file_size;
+ transmission_time=file_size/960;
+ cout<<" the time it takes to transmitte is"<<transmission_time<<" seconds ";
+ return 0;
+ 
+}
