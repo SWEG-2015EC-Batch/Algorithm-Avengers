a225b00266e612206228f512e45fd481d0753ac6
wengelawitgirma799 <141363872+wengelawitgirma799@users.noreply.github.com>
2023-11-17 23:45:17 +0300

    Create capcity.cpp

---
+++ ETS1344_15_wengelawit_girma/capcity.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int capacity, miles_per_gallon, miles_drivable;
+    cout<<" Enter capacity in gallon the tank can carry ";
+    cin>>capacity;
+    cout<< " Enter miles per gallon the automoblie can be driven ";
+    cin>>miles_per_gallon;
+    miles_drivable = capacity * miles_per_gallon;
+    cout<<" miles it can be driven = "<<miles_drivable<< " miles without refuling ";
+    return 0;
+}
