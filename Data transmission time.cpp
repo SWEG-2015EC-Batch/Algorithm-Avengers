e0d1684c91d7be5d53086ad8606819cd5d41b6a7
wintaatsbhaa <149393755+wintaatsbhaa@users.noreply.github.com>
2023-11-17 23:27:54 +0300

    Update

---
+++ Data transmission time.cpp
@@ -0,0 +1,9 @@
+#include<iostream.h>
+int main()
+{
+ float file_size, transmission_time;
+ cout<<" enter the size of the file ";
+ cin>>file_size;
+ transmission_time=file_size/960;
+ cout<<" the time it takes to transmitte is"<<transmission_time<<" seconds ";
+ }
