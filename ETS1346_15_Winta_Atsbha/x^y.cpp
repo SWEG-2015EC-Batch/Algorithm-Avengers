b055a2c2faef37a59dcb643730c3f8aab304f40c
wintaatsbhaa <149393755+wintaatsbhaa@users.noreply.github.com>
2023-11-17 23:34:01 +0300

    Update ETS1346_15_Winta_Atsbha

---
+++ ETS1346_15_Winta_Atsbha/x^y.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<math.h>
+int main()
+{
+ int X, Y;
+ cout<<" enter value for X ";
+ cin>>X;
+ cout<<" enter value for Y ";
+ cin>>Y;
+ int power= pow(X,Y);
+ cout<<" power of the given number is "<<power;
+ }
