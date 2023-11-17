05157ca213c57341c4cbc7725bba4620cc4a36d6
wengelawitgirma799 <141363872+wengelawitgirma799@users.noreply.github.com>
2023-11-17 23:29:45 +0300

    Create bmi.cpp

---
+++ ETS1344_15_wengelawit_girma/bmi.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ float weight, height, height_squared, BMI;
+ cout<<" Enter weight ";
+ cin>>weight;
+ cout<<" Enter height ";
+ cin>>height;
+ height_squared= height*height;
+ cout<<"height_squerd = "<<height_squared;
+ BMI=weight/height_squared;
+ cout<<" BMI of the person = "<<BMI;
+ return 0;
+}
