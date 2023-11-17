e6cc96895b2da932a6b6c96da37ccabd31b8d2be
THRESSA MENILEK <149164024+thressamenilekk@users.noreply.github.com>
2023-11-18 01:15:02 +0300

    Create Q2.cpp

---
+++ ETS1309_15_THRESSA_MENILEK/Ch2_Practical_Assignment1/Q2/Q2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float capacity, miles_per_gallon, miles_drivable;
+    cout<<" Enter capacity in gallon the tank can carry ";
+    cin>>Fuel_Tank_Capacity;
+    cout<< " Enter miles per gallon the automoblie can be driven ";
+    cin>>Fuel_Consumption_Rate;
+    Distance_Travelled = Fuel_Tank_Capacity * Fuel_Consumption_Rate;
+    cout<<" miles it can be driven = "<<Distance_Travelled<< " miles without refuling ";
+    return 0;
+}
