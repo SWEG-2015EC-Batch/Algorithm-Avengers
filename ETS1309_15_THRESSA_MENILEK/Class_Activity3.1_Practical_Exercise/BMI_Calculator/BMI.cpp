6bc3c91a08f4b4a4cf87bb22a96f5fa327517982
THRESSA MENILEK <149164024+thressamenilekk@users.noreply.github.com>
2023-12-17 16:37:14 +0300

    Update BMI.cpp

--- ETS1309_15_THRESSA_MENILEK/Class_Activity3.1_Practical_Exercise/BMI_Calculator/BMI.cpp
+++ ETS1309_15_THRESSA_MENILEK/Class_Activity3.1_Practical_Exercise/BMI_Calculator/BMI.cpp
@@ -1,13 +1,52 @@
-#include <iostream>
-using namespace std;
+#include<iostream.h>
 int main()
 {
-  float Weight, Height, BMI;
-  cout<<"enter Weight";
-  cin>>Weight;
-  cout<<"enter Height";
-  cin>>Height;
-  BMI = Weight/(Height * Height);
-  cout<<"The BMI of a body is"<<BMI;
-  return 0;
+	float weight, height, BMI;
+	char gender= 'f' ; 'm';
+	cout<<" enter weight ";
+	cin>>weight;
+	cout<<" enter height ";
+	cin>>height;
+	BMI= weight/(height*height);
+	cout<< " BMI of a  person = "<<BMI;
+	cout<<" enter gender ";
+	cin>> gender;
+	cout<<" your gender is "<<gender;
+	{
+	if('f')
+	{
+		if(BMI<18.5)
+		{
+		cout<<" under weight ";
+	}
+	    else if (BMI>=18.5 && BMI<24)
+	    {
+	    cout<<" normal weight ";
+	    }
+	    else if (BMI>=24 && BMI<30)
+	    {
+	    cout<<" over weight ";
+	    }
+	    else if (BMI>=30)
+	    cout<<" obese ";
+	}
+	}
+   {
+	   if('m')
+   {
+	   if(BMI<20.5)
+	   cout<<" under weight ";
+   }
+	   else if(BMI>=20.5 && BMI<25)
+	   {
+	   cout<<" normal weight ";
+	   }
+	   else if(BMI>=25 && BMI<30)
+	   {
+	   cout<<" over weight ";
+	   }
+	   else if(BMI>=30)
+	   cout<<" obese ";
+   
+	}
 }
