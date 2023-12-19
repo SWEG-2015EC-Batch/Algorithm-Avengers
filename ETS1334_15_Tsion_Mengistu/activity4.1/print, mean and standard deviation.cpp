89f35a93daec3ad416676f763e1e1ecbba9f70f8
TsionMengistu12 <149229052+TsionMengistu12@users.noreply.github.com>
2023-12-19 21:14:18 +0300

    Update print, mean and standard deviation.cpp

--- ETS1334_15_Tsion_Mengistu/activity4.1/print, mean and standard deviation.cpp
+++ ETS1334_15_Tsion_Mengistu/activity4.1/print, mean and standard deviation.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+int count=0,n,average,sum=0,x,variance,standardDeviation,total=0;
+cout<<"enter number of data to be computed ";
+cin>>n;
+for(;count<n;count++){
+cout<<"enter data ";
+cin>>x;
+sum+=x;
+total+=pow(x,2);
+}
+average=sum/n;
+cout<<" \n the mean of the data is "<<average;
+variance=(total/(n-1))-pow(average,2);
+standardDeviation=sqrt(variance);
+cout<<" \n the standard deviation is "<<standardDeviation;
+
+
+return 0;
+}
 
