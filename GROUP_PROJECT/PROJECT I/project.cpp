d50a033804e4c14449d8768134513bdf8d654591
TsionMengistu12 <149229052+TsionMengistu12@users.noreply.github.com>
2024-01-27 22:03:20 +0300

    Update project.cpp

--- GROUP_PROJECT/PROJECT I/project.cpp
+++ GROUP_PROJECT/PROJECT I/project.cpp
@@ -1,13 +1,14 @@
-
 #include <iostream>
 #include <string>
+#include<iomanip>
+using namespace std;
 
 const int MAX_CUSTOMERS = 100;
 
 struct Customer {
-    std::string name;
+    string name;
     char sex;
-    std::string address;
+    string address;
     int code;
     double totalSales;
     int numPurchases;
@@ -17,112 +18,158 @@ int main() {
     Customer customers[MAX_CUSTOMERS];
     int numCustomers = 0;
 
+cout<<" ***** WELCOME ****** "<<endl;
     while (true) {
-        std::cout << "Menu Options:\n"
-                     "1. Add new customer\n"
-                     "2. Search for a customer\n"
-                     "3. Record a purchase\n"
-                     "4. Generate detailed report\n"
-                     "5. Generate summary report\n"
-                     "6. Exit\n";
+        cout << "Menu Options: \n"
+                "1. Add new customer \n"
+                "2. Search for a customer \n"
+                "3. Record a purchase \n"
+                "4. End of day \n"
+                "5. Exit "<<endl;
         int choice;
-        std::cout << "Enter your choice: ";
-        std::cin >> choice;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-        if (choice == 1) {
-            std::string name, address;
-            char sex;
+        switch (choice) {
+            case 1: {
+                string name, address;
+                char sex;
 
-            std::cout << "Enter customer name: ";
-            std::cin >> name;
-            std::cout << "Enter customer sex (M/F): ";
-            std::cin >> sex;
-            std::cout << "Enter customer address: ";
-            std::cin >> address;
+                cout << "Enter customer name: ";
+                cin >> name;
 
-            bool isNewCustomer = true;
-            for (int i = 0; i < numCustomers; i++) {
-                if (customers[i].name == name) {
-                    isNewCustomer = false;
-                    break;
+                cout << "Enter customer sex (M/F): ";
+                cin >> sex;
+                if (sex!='m'||sex!='f'||sex!='M'||sex!='F'){
+                    cout<<" please enter in the appropriate format ( m or f ) ";
+
+                    return 0;
+
                 }
-            }
+                cout << "Enter customer address: ";
+                cin >> address;
 
-            if (isNewCustomer) {
-                customers[numCustomers].name = name;
-                customers[numCustomers].sex = sex;
-                customers[numCustomers].address = address;
-                customers[numCustomers].code = numCustomers + 1;
-                customers[numCustomers].totalSales = 0.0;
-                customers[numCustomers].numPurchases = 0;
-                numCustomers++;
-            }
-        } else if (choice == 2) {
-            std::cout << "Enter customer ID or name to search: ";
-            std::string searchName;
-            std::cin >> searchName;
-
-            bool found = false;
-            for (int i = 0; i < numCustomers; i++) {
-                if (customers[i].name == searchName || std::to_string(customers[i].code) == searchName) {
-                    found = true;
-                    std::cout << "Customer Details:\n"
-                                 "Name: " << customers[i].name << "\n"
-                                 "Sex: " << customers[i].sex << "\n"
-                                 "Address: " << customers[i].address << "\n"
-                                 "Customer Number: " << customers[i].code << "\n";
-                    break;
+                bool isNewCustomer = true;
+                for (int i = 0; i < numCustomers; i++) {
+                    if (customers[i].name == name) {
+                        isNewCustomer = false;
+                        break;
+                    }
                 }
-            }
-            if (!found) {
-                std::cout << "Customer not found.\n";
-            }
-        } else if (choice == 3) {
-            int customerCode, purchaseAmount;
-            std::cout << "Enter customer code: ";
-            std::cin >> customerCode;
-            std::cout << "Enter purchase amount: ";
-            std::cin >> purchaseAmount;
 
-            for (int i = 0; i < numCustomers; i++) {
-                if (customers[i].code == customerCode) {
-                    customers[i].totalSales += purchaseAmount;
-                    customers[i].numPurchases++;
-                    break;
+                if (isNewCustomer) {
+                    customers[numCustomers].name = name;
+                    customers[numCustomers].sex = sex;
+                    customers[numCustomers].address = address;
+                    customers[numCustomers].code = numCustomers + 1;
+                    customers[numCustomers].totalSales = 0.0;
+                    customers[numCustomers].numPurchases = 0;
+                    numCustomers++;
                 }
+                break;
             }
-        } else if (choice == 4) {
-            std::cout << "Detailed Report:\n";
-            for (int i = 0; i < numCustomers; i++) {
-                std::cout << "Customer Name: " << customers[i].name << "\n"
-                             "Sex: " << customers[i].sex << "\n"
-                             "Address: " << customers[i].address << "\n"
-                             "Customer Number: " << customers[i].code << "\n"
-                             "Total Sales: " << customers[i].totalSales << "\n"
-                             "Average Sales: " << (customers[i].totalSales / customers[i].numPurchases) << "\n";
-            }
-        } else if (choice == 5) {
-            double totalSales = 0.0;
-            int totalItemsSold = 0;
+            case 2: {
+                cout << "Enter customer ID or name to search: "<<endl;
+                string searchName;
+                cin >> searchName;
 
-            for (int i = 0; i < numCustomers; i++) {
-                totalSales += customers[i].totalSales;
-                totalItemsSold += customers[i].numPurchases;
+                bool found = false;
+                for (int i = 0; i < numCustomers; i++) {
+                    if (customers[i].name == searchName || to_string(customers[i].code) == searchName) {
+                        found = true;
+                        cout << "Customer Details:\n"
+                             << "Name:  " << customers[i].name << "\n"
+                             << "Sex:  " << customers[i].sex << "\n"
+                             << "Address:  " << customers[i].address << "\n"
+                             << "Customer Number:  " << customers[i].code << endl;
+                        break;
+                    }
+                }
+                if (!found) {
+                    cout << "Customer not found." << endl;
+                }
+                break;
             }
+            case 3: {
+                int customerCode, purchaseAmount;
+                cout << "Enter customer code: ";
+                cin >> customerCode;
+                cout << "Enter purchase amount: ";
+                cin >> purchaseAmount;
+                for (int i = 0; i < numCustomers; i++) {
+                    if (customers[i].code == customerCode) {
+                        customers[i].totalSales += purchaseAmount;
+                        customers[i].numPurchases++;
+                        break;
+                    }
+                }
+                break;
+            }
+            case 4: {
 
-            std::cout << "Summary Report:\n"
-                         "Number of Customers: " << numCustomers << "\n"
-                         "Total Sales: " << totalSales << "\n"
-                         "Total Items Sold: " << totalItemsSold << "\n";
-        } else if (choice == 6) {
-            break;
-        } else {
-            std::cout << "Invalid choice. Please try again.\n";
+            cout<<" ***** please choose Report format ***** \n"
+                  " 1. Detailed report \n"
+                  " 2. Summary report \n"
+                  " 3. exit"<<endl;
+
+            int choice2;
+            cin>>choice2;
+
+            switch(choice2){
+            case 1:{
+
+                cout << "........Detailed Report........\n";
+                cout << left<<setw(10) << "Customer Name: "
+                    << left<<setw(10)  << "Sex:  "
+                    << left<<setw(10)  << "Address:  "
+                    << left<<setw(10)  << "Customer Code:  "
+                    << left<<setw(10)  << "Total Sales:  "
+                    << left<<setw(10)  << "Average Sales:  "<<endl;
+                for (int i = 0; i < numCustomers; i++) {
+                    cout << left<<setw(15)<< customers[i].name
+                         << left<<setw(15)<< customers[i].sex
+                         << left<<setw(10)<< customers[i].address
+                         << left<<setw(15) << customers[i].code
+                         << left<<setw(15) << customers[i].totalSales
+                         << left<<setw(15) << (customers[i].totalSales / customers[i].numPurchases) << endl;
+                }
+                break;
+            }
+            case 2: {
+                double totalSales = 0.0;
+                int totalItemsSold = 0;
+
+                for (int i = 0; i < numCustomers; i++) {
+                    totalSales += customers[i].totalSales;
+                    totalItemsSold += customers[i].numPurchases;
+                }
+
+                cout << "........Summary Report........\n"
+                     << left<<setw(10) << "Number of Customers "
+                     << left<<setw(10) << "Total Sales "
+                     << left<<setw(10) << "Total Items Sold  "<<endl;
+
+                cout<< left<<setw(20) << numCustomers
+                    << left<<setw(20) << totalSales
+                    << left<<setw(20) << totalItemsSold << endl;
+                break;
+            }
+            case 3:{
+
+                return 0;
+
+            }
+            }
+            }
+            case 5: {
+                return 0;
+            }
+            default: {
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+            }
         }
     }
 
     return 0;
 }
-
-
-
