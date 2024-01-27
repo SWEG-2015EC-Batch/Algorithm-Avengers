ab9483a2234daae839411adc5e8b6b0447b234da
THRESSA MENILEK <149164024+thressamenilekk@users.noreply.github.com>
2024-01-27 11:29:12 +0300

    Delete Data transmission time.cpp

--- Data transmission time.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream.h>
-int main()
-{
- float file_size, transmission_time;
- cout<<" enter the size of the file ";
- cin>>file_size;
- transmission_time=file_size/960;
- cout<<" the time it takes to transmitte is"<<transmission_time<<" seconds ";
- }
