e6a5a8cba9f77e48b6d8e41b7a81788bb43efeac
TsionMengistu12 <149229052+TsionMengistu12@users.noreply.github.com>
2024-01-27 23:39:58 +0300

    Update Pattern.cpp

--- GROUP_PROJECT/PROJECT II/Pattern.cpp
+++ GROUP_PROJECT/PROJECT II/Pattern.cpp
@@ -1,33 +1,3 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() 
-    {
-    string str = "ABCDEFGHJ";
-    int n = str.size();
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (j <=i) {
-                cout << str[j]<<" ";
-            } else {
-                cout << "  ";
-            }
-        }
-
-        for (int j = n - 1; j >= 0; j--) {
-            if (j <= i) {
-                cout << str[j]<<" ";
-            } else {
-                cout << "  ";
-            }
-        }
-
-        cout << endl;
-    }
-
-  // a more efficient way to print it      
         
 #include <cstdlib>
 #include <iostream>
